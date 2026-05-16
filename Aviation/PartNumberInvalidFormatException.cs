78b0ca933b24059b0b59bc6e47cb774a553bfc50
Zach W. <zachwells@me.com>
2026-05-15 20:59:00 -0400

    Lab 3.2

--- Aviation/PartNumberInvalidFormatException.cs
+++ Aviation/PartNumberInvalidFormatException.cs
@@ -1,4 +1,10 @@
-﻿namespace com.ntier.Aviation;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ntier.Aviation;
 
 internal class PartNumberInvalidFormatException : Exception
 {
