4d7affad487c2926fea7a797e9fbb17f66d162d0
Zach W. <zachwells@me.com>
2026-05-15 20:58:53 -0400

    Lab 1.3

--- Aviation/AirplanePart.cs
+++ Aviation/AirplanePart.cs
@@ -1,49 +1,21 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace com.ntier.Aviation;
 
-internal abstract partial class AirplanePart: IComparable<AirplanePart>
+internal abstract class AirplanePart
 {
-    public string PartNumber
-    {
-        get => _partNumber;
-        set
-        {
-            if (value is null)
-            {
-                throw new PartNumberInvalidFormatException($"{nameof(PartNumber)} cannot be assigned to null");
-            }
-
-            if (illegalPartNumberChars().IsMatch(value))
-            {
-                throw new PartNumberInvalidFormatException($"Part number, \"{value}\", contains a space, \"*\", or \"?\"");
-            }
-
-            _partNumber = value;
-        }
-    }
-
+    public string PartNumber { get; set; } = "Default part name";
     public string Description { get; set; } = "Default desciption";
-    public double Price
-    { 
-        get => _price;
-        set => _price = value > 0 ? value : throw _negativePriceException;
+    public double Price { get; set; }
+
+    public virtual void GetPartInfo()
+    {
+        Console.WriteLine($"Part Number: {PartNumber}");
+        Console.WriteLine($"Description: {Description}" );
+        Console.WriteLine($"Price: {Price:C}");
     }
-
-    public int Count {  get; set; }
-    public int Threshold { get; set; }
-
-    private static readonly Exception _negativePriceException =
-        new ArgumentException("Price cannot be negative");
-
-    public double _price;
-    private string _partNumber = "Default part name";
-
-    [GeneratedRegex(@"[*?\s]")]
-    private static partial Regex illegalPartNumberChars();
-
-    public int CompareTo(AirplanePart? other) =>
-        other is not null
-        ? PartNumber.CompareTo(other.PartNumber)
-        : throw new NullReferenceException($"{nameof(AirplanePart)} is null");
 }
--- Aviation/EnginePart.cs
+++ Aviation/EnginePart.cs
@@ -1,8 +1,20 @@
-﻿namespace com.ntier.Aviation;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.ntier.Aviation;
 
 internal class EnginePart: AirplanePart, ISelfTest
 {
     public string EngineType { get; set; } = "Default engine type";
+
+    public override void GetPartInfo()
+    {
+        base.GetPartInfo();
+        Console.WriteLine($"Engine Type: {EngineType}");
+    }
 
     public int SelfTest()
     {
