bcee2bb0faa382cb77f31b462208b411e11b245e
Zach W. <zachwells@me.com>
2026-05-15 20:59:08 -0400

    Lab 4.5

--- Aviation/Program.cs
+++ Aviation/Program.cs
@@ -6,54 +6,21 @@ namespace com.ntier.Aviation;
 
 internal class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
         Program self = new();
-        string path = @"../../../Resources/parts.csv";
+        string path = @"C:\Users\h6\source\repos\Lab 4.2\Aviation\Resources\parts.csv";
+        EngineFactory factory = new();
+        List<AirplanePart> parts = [];
+        List<string> commands = [
+            "exit",
+            "list",
+            "get"
+        ];
 
-        var factoryTask = EngineManager.New(path, OnInventoryExhausted);
-
-        bool runloop;
-        do
-        {
-            runloop = await Execute(factoryTask);
-        }
-        while (runloop);
-
-        Console.WriteLine("Program exited");
-    }
-
-    private static async Task<bool> Execute(Task<EngineManager> factoryTask)
-    {
-        const string defaultPrompt = "Cmd: ";
-
-        ConsoleColor temp = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.Write(defaultPrompt);
-        Console.ForegroundColor = temp;
-
-        string[] input = (Console.ReadLine() ?? "").Split(' ');
-        var cmd = input.ParseCommand();
-
-        if (!factoryTask.IsCompleted)
-        {
-            temp = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Parts loading...");
-            Console.ForegroundColor = temp;
-        }
-
-        return await TryTask(cmd(factoryTask));
-    }
-
-    private static void OnInventoryExhausted(object _, InventoryEventArgs part)
-        => Console.WriteLine($"{part.PartNumber} is almost exausted\n");
-
-    private static async Task<T> TryTask<T>(Task<T> exceptionThrowable)
-    {
         try
         {
-            return await exceptionThrowable;
+            parts = [.. factory.LoadEngineParts(path)];
         }
         catch (FileNotFoundException ex)
         {
@@ -83,6 +50,83 @@ internal class Program
             Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
 
-        throw new Exception("Cannot procede due to unhandled exception");
+        string printableCommands = commands.Aggregate((a, b) => $"{a}, {b}");
+        string defaultPrompt =
+            $"""
+            Here are the available commands:
+                {printableCommands}
+            """;
+        bool runloop = true;
+        string? input = self.PromptUser(defaultPrompt);
+
+        while (runloop)
+        {
+            Func<string?> command = input switch
+            {
+                null => () => self.PromptUser($"Invalid Commmand\n{defaultPrompt}"),
+                var s when s == commands[0] => () =>
+                {
+                    runloop = false;
+                    Console.WriteLine("Program Complete");
+                    return "";
+                },
+                var s when s == commands[1] => () =>
+                {
+                    self.Print(factory.Cache.Select(kvp => kvp.Value.GetPartInfo()));
+                    return self.PromptUser(defaultPrompt);
+                },
+                var s when s.Contains(commands[2]) => () =>
+                {
+                    string partNumber = s.Split(' ')[1];
+                    if (factory.Cache.TryGetValue(partNumber, out var part))
+                    {
+                        Console.WriteLine(part.GetPartInfo());
+                        return self.PromptUser(defaultPrompt);
+                    }
+                    else
+                    {
+                        return self.PromptUser($"Could not find part: {partNumber}\n" +
+                                               $"{defaultPrompt}");
+                    }
+                    
+                },
+                _ => () => self.PromptUser($"Invalid Commmand\n{defaultPrompt}")
+            };
+
+            input = command();
+        }
+    }
+
+    private void Print(IEnumerable<string> list)
+    {
+        Console.WriteLine();
+        foreach (string item in list)
+        {
+            Console.WriteLine(item);
+        }
+        Console.WriteLine("-----");
+    }
+
+    private string? PromptUser(string prompt)
+    {
+        Console.WriteLine($"\n{prompt}");
+        return Console.ReadLine();
+    }
+
+    private static void TestException<T>(T? testParam,
+                                         Func<T> selector,
+                                         Action<T?> mutator)
+    {
+        T foo = selector();
+        try
+        {
+            mutator(testParam);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        mutator(foo);
     }
 }
