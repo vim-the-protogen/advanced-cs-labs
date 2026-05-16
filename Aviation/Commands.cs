c3d4608e995ea2cee7932b5c17f604243769368b
Zach W. <zachwells@me.com>
2026-05-15 20:59:14 -0400

    Lab 6.1

--- Aviation/Commands.cs
+++ Aviation/Commands.cs
@@ -1,88 +1,62 @@
-﻿namespace com.ntier.Aviation;
+﻿using System.Runtime.CompilerServices;
+
+//using Command = System.Func<com.ntier.Aviation.EngineFactory, bool>;
+namespace com.ntier.Aviation;
 
 internal static class Commands
 {
     private const string _helpMessage =
     $"""
-    help                                                    -  displays this message
-    list [price [ascending|descending|between <min> <max>]  -  display all parts. Optionally
-         [EngineType <Engine type>]                            sorted by price, ascending or
-                                                               descending, or can be filtered
-                                                               by price range or engine type
-    get <Part Number>                                       -  display the part that has <Part Number>
-    release <Part Number>                                   -  decrement the part count, and display or
-                                                               notify that the part is exausted
-    exit                                                    -  exit the program
+    help                                                   -  displays this message
+    list [price [ascending|descending|between <min> max>]  -  display all parts. Optionally
+         [EngineType <Engine type>]                           sorted by price, ascending or
+                                                              descending, or can be filtered
+                                                              by price range or engine type
+    get <Part Number>                                      -  display the part that has <Part Number>
+    release <Part Number>                                  -  decrement the part count, and display or
+                                                              notify that the part is exausted
+    exit                                                   -  exit the program
     """;
 
-    /// <summary>
-    /// Parse the command that is represented by <paramref name="args"/>
-    /// </summary>
-    /// <param name="args">
-    /// The input command
-    /// </param>
-    /// <returns>
-    /// An asynchronous function that returns a bool depending on whether the
-    /// program should exit, e.g. the "exit" command
-    /// </returns>
-    public static Func<Task<EngineManager>, Task<bool>> ParseCommand(this string[] args)
-        => args switch
+    public static Func<EngineFactory, bool> Execute(this string[] args) =>
+        args switch
         {
-            ["exit"] => _ => Task.FromResult("").Write(false),
+            ["exit"] => args.ConstructCommand(ExitCommand, false),
             ["get", _] => args.ConstructCommand(GetCommand, true),
             ["list", ..] => args.ListCommands(),
             ["release", _] => args.ConstructCommand(ReleaseCommand, true),
-            ["help"] => _ => Task.FromResult(_helpMessage).Write(true),
-            [""] => _ => Task.FromResult(string.Empty).Write(true),
-            _ => _ => args.InvalidCommand().Write(true),
+            ["help"] => _ => _helpMessage.Write(true),
+            [""] => _ => string.Empty.Write(true),
+            _ => args.InvalidCommand(),
         };
 
-    public static Func<Task<EngineManager>, Task<bool>> ListCommands(this string[] args)
+    public static Func<EngineFactory, bool> ListCommands(this string[] args)
         => args switch
         {
             [_] => args.ConstructCommand(ListCommand, true),
-            [_, "EngineType", _]
-                => args.ConstructCommand(ListByEngineType, true),
-            [_, "price", "between", _, _]
-                => args.ConstructCommand(ListByPriceBetween, true),
-            [_, "price", "ascending"] 
-                => args.ConstructCommand(ListByPriceAscendingCommand, true),
-            [_, "price", "descending"]
-                => args.ConstructCommand(ListByPriceDescendingCommand, true),
-            _ => _ => args.InvalidCommand().Write(true),
+            [_, "EngineType", _] => args.ConstructCommand(ListByEngineType, true),
+            [_, "price", "between", _, _] => args.ConstructCommand(ListByPriceBetween, true),
+            [_, "price", "ascending"] => args.ConstructCommand(ListByPriceAscendingCommand, true),
+            [_, "price", "descending"] => args.ConstructCommand(ListByPriceDescendingCommand, true),
+            _ => args.InvalidCommand()
         };
-
-    private static Func<Task<EngineManager>, Task<bool>>
-        ConstructCommand(this string[] args,
-                         Func<Task<EngineManager>, string[], Task<string>> cmd,
-                         bool result)
-        => factory => cmd(factory, args).Write(result);
-
-    private static async Task<bool> Write(this Task<string> output, bool result)
-        => await (output.Result).Write(result);
-
-    private static Task<bool> Write(this string output, bool result)
-    {
-        Console.WriteLine($"{output}");
-        return Task.FromResult(result);
-    }
 
     private static bool EngineTypeMatches(this string engineType,
                                           EnginePart? part)
         => part is not null &&
            part.EngineType.Equals(engineType,
                                   StringComparison.CurrentCultureIgnoreCase);
-    private static async Task<string> ListByEngineType(Task<EngineManager> parts,
-                                                       string[] args)
-        => (await parts).Where(p => p is EnginePart)
-                        .Select(p => p as EnginePart)
-                        .Where(args[2].EngineTypeMatches)
-                        .Select(p => p.GetPartInfo("\t"))
-                        .Prepend($"Parts with Engine Type, {args[2]}:")
-                        .Print();
+    private static string ListByEngineType(this EngineFactory parts,
+                                           string[] args)
+        => parts.Where(p => p is EnginePart)
+                .Select(p => p as EnginePart)
+                .Where(args[2].EngineTypeMatches)
+                .Select(p => p.GetPartInfo("\t"))
+                .Prepend($"Parts with Engine Type, {args[2]}:")
+                .Print();
 
-    private static async Task<string> ListByPriceBetween(Task<EngineManager> parts,
-                                                         string[] args)
+    private static string ListByPriceBetween(this EngineFactory parts,
+                                             string[] args)
     {
         if (!double.TryParse(args[3], out double min))
         {
@@ -99,43 +73,59 @@ internal static class Commands
             return $"Error: min, {min}, is greater than max, {max}";
         }
 
-        return (await parts).Where(p => p.Price >= min && p.Price <= max)
-                            .Select(p => p.GetPartInfo("\t"))
-                            .Print();
+        return parts.Where(p => p.Price >= min && p.Price <= max)
+                    .Select(p => p.GetPartInfo("\t"))
+                    .Print();
     }
 
+    private static Func<EngineFactory, bool> InvalidCommand(this string[] args)
+        => _ => $"Invalid Command: {string.Join(' ', args)}".Write(true);
 
-    private static Task<string> InvalidCommand(this string[] args)
-        => Task.FromResult($"Invalid Command: {string.Join(' ', args)}");
+    private static Func<EngineFactory, bool>
+        ConstructCommand(this string[] args,
+                        Func<EngineFactory, string[], string> cmd,
+                        bool result)
+        => factory => cmd(factory, args).Write(result);
 
-    private static async Task<string> ReleaseCommand(Task<EngineManager> parts,
-                                                     string[] args)
-        => (await parts).Release(args[1]) is AirplanePart part
+    private static bool Write(this string output, bool result)
+    {
+        Console.WriteLine($"{output}");
+        return result;
+    }
+
+    private static string ReleaseCommand(this EngineFactory parts,
+                                         string[] args)
+        => parts.Release(args[1]) is AirplanePart part
            ? part.GetPartInfo("\t")
            : $"The part, {args[1]}, is exhausted";
 
-    private static async Task<string> GetCommand(Task<EngineManager> parts,
-                                                 string[] args)
-        => (await parts).FirstOrDefault(p => p.PartNumber == args[1]) is AirplanePart part
+
+    private static string ExitCommand(this IEnumerable<AirplanePart> parts,
+                                      string[] args)
+        => "Program Complete";
+
+    private static string GetCommand(this IEnumerable<AirplanePart> parts,
+                                     string[] args)
+        => parts.FirstOrDefault(p => p.PartNumber == args[1]) is AirplanePart part
            ? part.GetPartInfo("\t")
            : $"Error: Could not find part, {args[1]}";
 
-    private static async Task<string>
-        ListByPriceAscendingCommand(Task<EngineManager> parts,
+    private static string
+        ListByPriceAscendingCommand(this IEnumerable<AirplanePart> parts,
                                     string[] args)
-            => (await parts).OrderBy(ap => ap.Price)
-                            .Select(ap => ap.GetPartInfo("\t"))
-                            .Prepend("Parts by price, ascending:")
-                            .Print();
+            => parts.OrderBy(ap => ap.Price)
+                    .Select(ap => ap.GetPartInfo("\t"))
+                    .Prepend("Parts by price, ascending:")
+                    .Print();
 
-    private static async Task<string>
-        ListByPriceDescendingCommand(Task<EngineManager> parts,
+    private static string
+        ListByPriceDescendingCommand(this IEnumerable<AirplanePart> parts,
                                      string[] _)
-        => (await parts).OrderBy(ap => ap.Price)
-                        .Reverse()
-                        .Select(ap => ap.GetPartInfo("\t"))
-                        .Prepend("Parts by price, ascending:")
-                        .Print();
+        => parts.OrderBy(ap => ap.Price)
+                .Reverse()
+                .Select(ap => ap.GetPartInfo("\t"))
+                .Prepend("Parts by price, ascending:")
+                .Print();
 
     /// <summary>
     /// Generate a <see cref="string"/> containing all
@@ -143,11 +133,11 @@ internal static class Commands
     /// </summary>
     /// <param name="parts">Storer of the database</param>
     /// <returns></returns>
-    private static async Task<string> ListCommand(Task<EngineManager> parts,
-                                                  string[] _)
-        => (await parts).Select(p => p.GetPartInfo("\t"))
-                        .Prepend("Parts by part number, ascending:")
-                        .Print();
+    private static string ListCommand(this IEnumerable<AirplanePart> parts,
+                                      string[] _)
+        => parts.Select(p => p.GetPartInfo("\t"))
+                .Prepend("Parts by part number, ascending:")
+                .Print();
 
     private static string Print(this IEnumerable<string> list)
         => $"{(list.Any()
--- Aviation/Program.cs
+++ Aviation/Program.cs
@@ -6,54 +6,19 @@ namespace com.ntier.Aviation;
 
 internal class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
         Program self = new();
         string path = @"../../../Resources/parts.csv";
+        EngineFactory factory = new([]);
+        List<AirplanePart> parts = [];
+        factory.InventoryExhausted +=
+           (_, a) => Console.WriteLine($"{a.PartNumber} " +
+                                       $"is almost exausted\n");
 
-        var factoryTask = EngineManager.New(path, OnInventoryExhausted);
-
-        bool runloop;
-        do
-        {
-            runloop = await Execute(factoryTask);
-        }
-        while (runloop);
-
-        Console.WriteLine("Program exited");
-    }
-
-    private static async Task<bool> Execute(Task<EngineManager> factoryTask)
-    {
-        const string defaultPrompt = "Cmd: ";
-
-        ConsoleColor temp = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.Write(defaultPrompt);
-        Console.ForegroundColor = temp;
-
-        string[] input = (Console.ReadLine() ?? "").Split(' ');
-        var cmd = input.ParseCommand();
-
-        if (!factoryTask.IsCompleted)
-        {
-            temp = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Parts loading...");
-            Console.ForegroundColor = temp;
-        }
-
-        return await TryTask(cmd(factoryTask));
-    }
-
-    private static void OnInventoryExhausted(object _, InventoryEventArgs part)
-        => Console.WriteLine($"{part.PartNumber} is almost exausted\n");
-
-    private static async Task<T> TryTask<T>(Task<T> exceptionThrowable)
-    {
         try
         {
-            return await exceptionThrowable;
+            parts = [.. factory.LoadEngineParts(path)];
         }
         catch (FileNotFoundException ex)
         {
@@ -83,6 +48,27 @@ internal class Program
             Console.WriteLine($"An unexpected error occurred: {ex.Message}");
         }
 
-        throw new Exception("Cannot procede due to unhandled exception");
+        bool runloop;
+        do
+        {
+            runloop = self.Execute(factory);
+        }
+        while (runloop);
+    }
+
+    private bool Execute(EngineFactory factory)
+    {
+        const string defaultPrompt = "Cmd: ";
+
+        ConsoleColor temp = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(defaultPrompt);
+        Console.ForegroundColor = temp;
+
+        string[] input = (Console.ReadLine() ?? "").Split(' ');
+        var cmd = input.Execute();
+        
+
+        return cmd(factory);
     }
 }
