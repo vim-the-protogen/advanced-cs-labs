7e8b7d2c68ade9a2968086b5f7ebd4e8c447934e
Zach W. <zachwells@me.com>
2026-05-15 21:02:26 -0400

    removing things that shouldn't be there

--- Aviation/InventoryEventArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace com.ntier.Aviation;
-
-delegate void InventoryEventHandler(object sender, InventoryEventArgs e);
-
-internal class InventoryEventArgs(string partNumber) : EventArgs
-{
-    public string PartNumber => partNumber;
-}
--- Aviation/PartNumberInvalidFormatException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace com.ntier.Aviation;
-
-internal class PartNumberInvalidFormatException : Exception
-{
-    public PartNumberInvalidFormatException(string message) : base(message) { }
-    public PartNumberInvalidFormatException(string message,
-                                      Exception innerException) :
-        base(message, innerException) { }
-}
